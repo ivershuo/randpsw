93706754f874265546de23b6e7037116b10df445
ivershuo <shuo@aerfa.net>
2013-09-18 00:24:04 +0800

    add domain map

--- background.js
+++ background.js
@@ -1,19 +1,63 @@
 (function(){
 	'use strict';
 
+	var LOCAL_SALT_KEY = 'salt',
+		LOCAL_DOMAINMAP_KEY = 'domainmap';
+
 	var runtime = chrome.runtime,
-		salt = localStorage.salt || '',
 		pageAction = chrome.pageAction;
 
-	function setIconStatus(tabId, lock){
-	    var icon = lock ? 'lock' : 'open';
+	var salt,domainMap;
+
+	var Utils = ({
+		_getDomain : function(){
+			var aElement = document.createElement('a');
+			return function(uri){
+				if(!uri){
+					return '';
+				}
+				if(!/^\w{3,5}\:\/\//.test(uri)){
+					uri = 'http://' + uri;
+				}
+				aElement.href = uri;
+				var domain;
+				try{
+					var domain = aElement.hostname.match(/\w+\.\w+$|\w+$|(\d{1,3}\.){3}\d{1,3}$/)[0].toLowerCase();
+				}catch(e){
+					domain = '';
+				}
+				return domain;
+			}		
+		},
+		init : function(){
+			this.getDomain = this._getDomain();
+			delete this.init;
+			return this;
+		}
+	}).init();
+
+	var setIconStatus = function(tabId, lock){
+		var icon = lock ? 'lock' : 'open';
 	    pageAction.setIcon({tabId: tabId, path : 'imgs/' + icon + '.png'});
 	    pageAction.show(tabId);
 	}
 
+	var localData = {
+		set : function(k, v){
+			v = typeof v == 'string' ? v : JSON.stringify(v);
+			localStorage.setItem(k, v);
+		},
+		get : function(k){
+			var v = localStorage.getItem(k) || '';
+			return /^\{/.test(v) ? JSON.parse(v) : v;
+		}
+	};
+
 	var actions = {
 		getPasswd : function(value, url, sender, cb){
-			var passwd = randpsw(value, url, salt);
+			var domain = Utils.getDomain(url);
+			domain = domainMap[domain] || domain;
+			var passwd = randpsw(value, domain, salt);
 			cb({
 				passwd  : passwd,
 				hasSalt : !!salt
@@ -21,21 +65,53 @@
 		},
 		setIcon : function(lock, sender, cb){
 			setIconStatus(sender.tab.id, lock);
+		},
+		saveSalt : function(salt, sender, cb){
+			salt = hex_md5(salt);
+			localData.set(LOCAL_SALT_KEY, salt);		
+		},
+		updateDomainMap : function(){			
+			localData.set(LOCAL_DOMAINMAP_KEY, domainMap);
+			chrome.runtime.sendMessage({
+				action : 'domainMapUpdated',
+				data   : domainMap
+			});
+		},
+		saveDomainMap : function(domain, bintoDomain, sender, cb){
+			if(!domain || !bintoDomain){
+				return;
+			}
+			domain      = Utils.getDomain(domain);
+			bintoDomain = Utils.getDomain(bintoDomain);
+			if(domainMap[domain]){
+				return;
+			}
+			domainMap[domain] = bintoDomain;
+			this.updateDomainMap();
+		},
+		deleteDomainMap : function(domain){
+			if(!domain || !domainMap[domain]){
+				return;
+			}
+			delete domainMap[domain];
+			this.updateDomainMap();
+		},
+		getDomainMap : function(sender, cb){
+			cb(localData.get(LOCAL_DOMAINMAP_KEY));
 		}
+	};
+
+	var run = function(){
+		salt = localData.get(LOCAL_SALT_KEY);
+		domainMap = localData.get(LOCAL_DOMAINMAP_KEY) || {};
+
+		/*监听给插件的消息*/
+		runtime.onMessage.addListener(function(data, sender, cb){
+			if(data && data.action && actions[data.action]){
+				var params = (data.params || []).concat(sender, cb);
+				actions[data.action].apply(actions, params);
+			}
+		});
 	}
-
-	/*监听给插件的消息*/
-	runtime.onMessage.addListener(function(data, sender, cb){
-		if(data && data.action && actions[data.action]){
-			var params = (data.params || []).concat(sender, cb);
-			actions[data.action].apply(actions, params);
-		}
-	});
-
-	/*监听salt值改变*/
-	window.addEventListener("storage", function(e){
-	    if(e.key == 'salt'){
-	    	salt = localStorage.salt;
-	    }
-	}, false);
+	run();
 })();
--- options.js
+++ options.js
@@ -5,20 +5,67 @@
         return typeof(v) == 'string' ? document.getElementById(v) : v;
     }
 
-	function saveSalt(salt){
-		localStorage.setItem('salt', hex_md5($('salt').value));
+    var mDomain = $('domain'),
+    	mBindto = $('bindto');
+	
+	var messageSender = function(actions, params, cb){
+    	chrome.runtime.sendMessage({
+    		action : actions,
+    		params : params
+    	}, function(response) {
+			cb && cb(response);
+		});
+    };
+
+    var domainMapTmpl = '<li><span class="unmap" data-domain="$1">删</span><strong>$1</strong> => <span>$2</span></li>',
+    	domainListEl  = $('domainmap-list');
+    var fillDomainMap = function(domainData){
+    	if(!domainData){
+    		return;
+    	}
+    	var html = [];
+    	for(var domain in domainData){
+    		html.unshift(domainMapTmpl.replace(/\$1/g, domain).replace('$2', domainData[domain]));
+    	}
+    	domainListEl.innerHTML = html.join('');
+    }
+
+	var eventMap = {
+		'#save:click' : function(){
+			messageSender('saveSalt', [$('salt').value]);
+		},
+		'#query:click' : function(){
+			messageSender('getPasswd', [$('psw').value, $('url').value], function(response) {
+				$('randpsw').value = response.passwd;
+			});
+		},
+		'#map:click' : function(){
+			messageSender('saveDomainMap', [mDomain.value, mBindto.value]);
+		},
+		'#domainmap-list:click' : function(e){
+			var target = e.target,
+				isDeleteEl = Array.prototype.indexOf.call(target.classList, 'unmap') > -1;
+			if(isDeleteEl){
+				messageSender('deleteDomainMap', [target.dataset.domain]);
+			}	
+		}
+	};
+
+	for(var ele in eventMap){
+		var func = eventMap[ele],
+			d    = ele.split(':'),
+			el   = document.querySelector(d[0]),
+			evt  = d[1];
+		el.addEventListener(evt, func);
 	}
 
-	$('save').addEventListener('click', function(){
-		saveSalt($('salt').value);
-    });
+	chrome.runtime.onMessage.addListener(function(data, sender, cb){
+		if(data.action && data.action == 'domainMapUpdated'){
+			fillDomainMap(data.data);
+		}
+	});
 
-    $('query').addEventListener('click', function(){
-    	chrome.runtime.sendMessage({
-    		action: "getPasswd",
-    		params : [$('psw').value, $('url').value]
-    	}, function(response) {
-			$('randpsw').value = response.passwd;
-		});
-    });
+	messageSender('getDomainMap', [], function(data){
+		fillDomainMap(data);
+	});	
 })();
